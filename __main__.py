a2101e581da0a0d5917deed3914722361a92c22f
Kennedy Mindeman <kennedymindeman@gmail.com>
2024-02-15 10:32:34 -0600

    Prevent program from continuing after pygame is done

--- __main__.py
+++ __main__.py
@@ -1,25 +1,30 @@
+import sys
 import pygame
 
 
 class Screen:
     def __init__(self, width, height):
+        pygame.init()
         self.width = width
         self.height = height
-        pygame.init()
         self.screen = pygame.display.set_mode((self.width, self.height))
 
 
 def main():
     screen = Screen(800, 600)
+    while True:
+        update()
+
+
+def update():
     event_loop()
 
 
 def event_loop():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
 
 if __name__ == "__main__":

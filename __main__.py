db1fdaae6e3b7ac3d0b405ddc7285e59b97b1a52
Kennedy Mindeman <kennedymindeman@gmail.com>
2024-02-15 12:14:16 -0600

    Add support for loading maps from json files

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import pygame
 
 
@@ -8,12 +9,36 @@ class Display:
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
+        self.tile_width = 60
+        self.tile_height = 60
+
+    def draw_tile(self, x, y, color):
+        upper_left_x = x * self.tile_width
+        upper_left_y = y * self.tile_height
+
+        rect = (upper_left_x, upper_left_y, self.tile_width, self.tile_height)
+        pygame.draw.rect(self.screen, color, rect)
+
+    def draw_map(self, map_data):
+        for y, row in enumerate(map_data['tiles']):
+            for x, tile in enumerate(row):
+                if tile['land']:
+                    self.draw_tile(x, y, pygame.Color('green'))
+                else:
+                    self.draw_tile(x, y, pygame.Color('blue'))
 
 
 def main():
     display = Display(800, 600)
+    map_data = read_json_map_file('maps/dummy_map.json')
+    display.draw_map(map_data)
     while True:
         update(display)
+
+
+def read_json_map_file(file_path):
+    with open(file_path) as json_map_file:
+        return json.load(json_map_file)
 
 
 def update(display):

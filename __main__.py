185491b60f00f0c23b7b431883c8b1c92a26826e
Kennedy Mindeman <kennedymindeman@gmail.com>
2024-02-15 10:17:23 -0600

    Add pygame boilerplate

--- __main__.py
+++ __main__.py
@@ -1,5 +1,25 @@
+import pygame
+
+
+class Screen:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+
 def main():
-    pass
+    screen = Screen(800, 600)
+    event_loop()
+
+
+def event_loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
 
 
 if __name__ == "__main__":
